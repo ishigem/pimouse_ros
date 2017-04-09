084c9512863626cbe0844bfbdcceaf69222235f2
ishigem <ishigem@gmail.com>
2017-04-09 22:45:15 +0900

    LightSenserTest 7.5

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -15,12 +15,12 @@ class LightsensorTest(unittest.TestCase):
         self.count += 1
         self.values = data
     
-    def check_values(self,lf,ls,rf,rs):
+    def check_values(self,lf,ls,rs,rf):
         vs = self.values
         self.assertEqual(vs.left_forward, lf, "different value: left_forward")
         self.assertEqual(vs.left_side, ls, "different value: left_side")
-        self.assertEqual(vs.right_forward, rf, "different value: right_forward")
         self.assertEqual(vs.right_side, rs, "different value: right_side")
+        self.assertEqual(vs.right_forward, rf, "different value: right_side")
         self.assertEqual(vs.sum_all, lf+ls+rs+rf, "different value: sum_all")
         self.assertEqual(vs.sum_forward, lf+rf, "different value: sum_forward")
 
