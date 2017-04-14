aa357218bc7f34b2342ba9cafabff66b40176ec4
ishigem <ishigem@gmail.com>
2017-04-14 13:14:23 +0900

    buzzer action test

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -8,7 +8,7 @@ from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback, MusicGoal
 
 class BuzzerTest(unittest.TestCase):
     def setUp(self):
-        self.client = actionlib.SimpleActionClient("Music", MusicAction)
+        self.client = actionlib.SimpleActionClient("music", MusicAction)
         self.device_values = []
 
     def test_node_exist(self):
@@ -40,7 +40,7 @@ class BuzzerTest(unittest.TestCase):
         ###preemption###
         self.device_values = []
         self.client.send_goal(goal, feedback_cb=self.feedback_cb)
-        self.client.wait_for_result(rospy.duration.from_sec(0.5))
+        self.client.wait_for_result(rospy.Duration.from_sec(0.5))
 
         self.assertFalse(self.client.get_result(), "stop is requested but return true")
         self.assertFalse(goal.freqs == self.device_values, "not stopped")
